7b435c7c05ad626def8324534a534bfd59ca5822
Elijah Andreev <ilyxi4@mail.ru>
2014-04-10 15:59:35 +0300

    Some tests for MailServiceImpl

--- qsm/test/org/hillel/it/qsm/service/impl/MailServiceImplTest.java
+++ qsm/test/org/hillel/it/qsm/service/impl/MailServiceImplTest.java
@@ -3,6 +3,7 @@ package org.hillel.it.qsm.service.impl;
 import static org.junit.Assert.*;
 
 import org.hillel.it.qsm.model.entities.Message;
+import org.hillel.it.qsm.model.entities.User;
 import org.hillel.it.qsm.persistance.memory.UsersRepository;
 import org.hillel.it.qsm.persistance.repository.UserRepository;
 import org.hillel.it.qsm.service.MailService;
@@ -10,6 +11,27 @@ import org.junit.Test;
 
 public class MailServiceImplTest {
 
+	@Test
+	public void MailServiceImplConstructorTest() {
+		UserRepository userRepository = new UsersRepository();
+
+		MailService service = new MailServiceImpl("example@qsm.com", "123xxx",
+				userRepository);
+		MailService service1 = new MailServiceImpl("example@qsm.com", "123xxx",
+				userRepository);
+		MailService service2 = new MailServiceImpl("example@qsm.com", "1232xx",
+				userRepository);
+		
+	}
 	
+	@Test
+	public void sendMessageTest() {
+		UserRepository userRepository = new UsersRepository();
+
+		MailService service = new MailServiceImpl("example@qsm.com", "123xxx",
+				userRepository);
+		service.sendMessage("theme", "mail@mail.com", "text");
+		
+	}
 
 }
