8e82ed2595af27361ee84209364a32d371ec3fd2
Galusik <gpotemkina@mail.ru>
2014-11-20 13:10:15 +0200

    test

---
+++ src/TestExec.java
@@ -0,0 +1,37 @@
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+//public class TestExec extends TestCase{
+	public class TestExec{
+		Map<String, String> expected;
+		Map<String, String> result;	
+
+	@Before
+	public  void setUpBeforeClass() throws Exception {
+		String args[]={"--cmd=send_message","--user_id=4897","--message=Hello"};
+		
+		
+		this.result=Exec.parsingArgs(args);
+		this.expected = new LinkedHashMap<>();
+		
+		expected.put("--cmd", "send_message");
+		expected.put("--user_id", "4897");
+		expected.put("--message", "Hello");
+		
+	}
+
+	@Test
+	public void test() {
+		assertEquals(expected, result);
+	}
+
+
+	}
+
+
