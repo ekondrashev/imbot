b505098bc8691a973755c4ff5915c1637b4548dc
Alex Potemkin <potemkinhaker@gmail.com>
2014-11-21 16:37:26 +0200

    Add pattern metod in void DecodeToMap and TestCase for check

--- CommandLine/src/Exec.java
+++ CommandLine/src/Exec.java
@@ -4,78 +4,152 @@ import java.io.InputStreamReader;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Exec {
 
 	private static final String USAGE = "Usage: Exec --cmd=<os_cmd> -param1=<value> -param2=<value>";
 
-	public static Map<String, String> decodeToMap(String args[]) {
-		Map<String, String> ourResult = new LinkedHashMap<>(); 
-		 
-		String cmdSignature = "--cmd=";
-		String cmdHelp = "--help";
-		String key,value;
-		String errorkey="Error";
-		String errorvalue="Error input command!!!";
-		
-		//первый проход
-		if (args.length>0){
-			if (args[0].startsWith(cmdSignature)) {
-				value=args[0].substring(cmdSignature.length(),args[0].length());
-				key="command";
-			    ourResult.put(key, value);	
-			}
-			
-			else if (args[0].startsWith(cmdHelp)) {
-				value=USAGE;
-				key="help";
-			    ourResult.put(key, value);	
-			    return ourResult;
-			}
-			else
-			{
-			    ourResult.put(errorkey, errorvalue);
-			    return ourResult;
-			}
-				
-		}
-		
-		//для остального ищем первый минус и потом равно
-		for (int i=1; i<=args.length-1 ;i++) {
-			String arg=args[i];
-			
-			int posMinus=arg.indexOf("-");
-			int posEquals=arg.indexOf("=");
-			
-			if (posMinus==-1 || posEquals==-1) {
-				ourResult.put(errorkey, errorvalue);
-				break;
-			}  
-			
-			key=arg.substring(posMinus+1,posEquals);
-			value=arg.substring(posEquals+1,arg.length());
-			ourResult.put(key, value);
-		}
-		return ourResult;
+	
+	private static Matcher controlMyString(String pat, String forFind) //общая для красоты
+	{
+		Pattern patt = Pattern.compile(pat);
+		Matcher mat = patt.matcher(forFind);
+		return mat;
 	}
 	
+	
+	public static Map<String, String> decodeToMap(String args[], int variant) {
+		Map<String, String> ourResult = new LinkedHashMap<>();
+
+		String cmdSignature = "--cmd=";
+		String cmdHelp = "--help";
+		String key = null, value = null;
+		String errorkey = "Error";
+		String errorvalue = "Error input command!!!";
+
+		if (variant == 1) {
+
+			// первый проход
+			if (args.length > 0) {
+				if (args[0].startsWith(cmdSignature)) {
+					value = args[0].substring(cmdSignature.length(),
+							args[0].length());
+					key = "command";
+					ourResult.put(key, value);
+				}
+
+				else if (args[0].startsWith(cmdHelp)) {
+					value = USAGE;
+					key = "help";
+					ourResult.put(key, value);
+					return ourResult;
+				} else {
+					ourResult.put(errorkey, errorvalue);
+					return ourResult;
+				}
+
+			}
+
+			// для остального ищем первый минус и потом равно
+			for (int i = 1; i <= args.length - 1; i++) {
+				String arg = args[i];
+
+				int posMinus = arg.indexOf("-");
+				int posEquals = arg.indexOf("=");
+
+				if (posMinus == -1 || posEquals == -1) {
+					ourResult.put(errorkey, errorvalue);
+					break;
+				}
+
+				key = arg.substring(posMinus + 1, posEquals);
+				value = arg.substring(posEquals + 1, arg.length());
+				ourResult.put(key, value);
+			}
+			return ourResult;
+
+		}
+		// pattern
+		else {
+			if (args.length > 0) {
+				String myPatternCmd = "--[Cc][Mm][Dd]=.+$";
+				String myPatternArg = "=.+$";
+				String myPatternHelp = "--[Hh][Ee][Ll][Pp]$";
+				Matcher m;
+				if (controlMyString(myPatternHelp,args[0]).matches())
+				{
+					ourResult.put("help", USAGE);
+					return ourResult;
+				}	
+				else if (controlMyString(myPatternCmd,args[0]).matches())
+
+				{
+					m = controlMyString(myPatternArg, args[0]);
+					if (m.find())
+					{
+						key = "command";
+						ourResult.put(key, m.group().replaceAll("=", ""));
+					}
+					
+				}
+				else
+					{
+					ourResult.put(errorkey, errorvalue);
+					ourResult.put("help", USAGE);
+					return ourResult;
+					}
+				
+				
+				String myPatternCmdVerify = "-[a-zA-Z]+=.+$";
+				myPatternCmd = "-[a-zA-Z]+=";				
+				myPatternArg = "=.+$";
+				
+				
+				for (int i = 1; i < args.length; i++)
+				{
+					String arg = args[i];
+					
+					m = controlMyString(myPatternCmdVerify, arg);
+					
+					if (m.matches()) {
+						
+						m = controlMyString(myPatternCmd, arg);
+						if (m.find()) key=m.group(); else key=errorkey;
+						
+						m = controlMyString(myPatternArg, arg);
+						if (m.find()) value=m.group(); else value=errorvalue;
+						
+						key=key.replaceAll("-", "").replaceAll("=", "").toLowerCase();
+						value=value.replaceAll("=", "").toLowerCase();
+						ourResult.put(key, value);
+						
+						if (key==errorkey) break; 
+					}
+				}
+			}
+			else ourResult.put("help", USAGE);
+
+			return ourResult;
+		}
+	}
+
 	public static void main(String args[]) {
 
-		Map<String, String> ourResult = decodeToMap(args);
-		
+		Map<String, String> ourResult = decodeToMap(args, 1);
+
 		if (ourResult.size() == 0) {
 			System.out.println(USAGE);
-	
-		}
-		else if (ourResult.containsKey("Error")) {
-			
+
+		} else if (ourResult.containsKey("Error")) {
+
 			System.out.println(ourResult.get("Error"));
-		}
-		else {
+		} else {
 			for (Entry<String, String> elem : ourResult.entrySet()) {
-	
-		        System.out.println(elem.getKey() +":" + elem.getValue());           
-		    }
+
+				System.out.println(elem.getKey() + ":" + elem.getValue());
+			}
 		}
 
 	}
@@ -83,9 +157,10 @@ public class Exec {
 	@SuppressWarnings("unused")
 	private static void executeCmd(String cmd) throws InterruptedException {
 		Runtime runtime = Runtime.getRuntime();
-		
+
 		try {
-			Process process = runtime.exec(new String[] { "cmd.exe", "/c",  cmd });
+			Process process = runtime
+					.exec(new String[] { "cmd.exe", "/c", cmd });
 			process.waitFor();
 			BufferedReader bReader = new BufferedReader(new InputStreamReader(
 					process.getInputStream()));
--- CommandLine/src/TestMyExec.java
+++ CommandLine/src/TestMyExec.java
@@ -1,6 +1,5 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import junit.framework.TestCase;
 
 import org.junit.Test;
@@ -13,7 +12,7 @@ public class TestMyExec extends TestCase{
 		
 		String args[]={"--cmd=sendmessage","-userid=vasia","-pass=123456"}; //My parameters 
 		
-		Map<String,String> myHashFromExec=Exec.decodeToMap(args);
+		Map<String,String> myHashFromExec=Exec.decodeToMap(args,1);
 
 		Map<String,String> myExample= new LinkedHashMap<>();
 
@@ -32,7 +31,7 @@ public class TestMyExec extends TestCase{
 		String args[]={"--help"}; //My parameters 
 		String USAGE = "Usage: Exec --cmd=<os_cmd> -param1=<value> -param2=<value>";
 		
-		Map<String,String> myHashFromExec=Exec.decodeToMap(args);
+		Map<String,String> myHashFromExec=Exec.decodeToMap(args,1);
 
 		Map<String,String> myExample= new LinkedHashMap<>();
 
@@ -42,5 +41,20 @@ public class TestMyExec extends TestCase{
 		
 	}
 
+	
+	@Test
+	public final void testControlHelpPattern() {
+		String args[]={"--cmd=sendmessage","-userid=vasia","-pass=123456",}; //My parameters 
+		
+		Map<String,String> myHashFromExec=Exec.decodeToMap(args,0);
+		
+		Map<String,String> myExample= new LinkedHashMap<>();
+		myExample.put("command","sendmessage");
+		myExample.put("userid","vasia");
+		myExample.put("pass","123456");
+		
+		assertEquals(myHashFromExec, myExample);
+		
+	}
 
 }
