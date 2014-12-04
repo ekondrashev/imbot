c2b425cff454686e389b2510e3e248b4db0c21a8
Galusik <gpotemkina@mail.ru>
2014-12-04 12:36:59 +0200

    refactoring

--- src/Exec.java
+++ src/Exec.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,68 +6,63 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-
 public class Exec {
 
 	public static final String USAGE = "Usage: Exec --cmd=send_message --user_id=<id> --message=<message>";
-	private static final String regexpCMD = "--(cmd)=(.+)$";
 	public static final String pat = "--(.+)=(.+)$";
 	static Pattern p = Pattern.compile(pat);
-	
-	
+
 	public static Map<String, String> parsingArgs(String[] args) {
-		    Map<String, String> map = new LinkedHashMap<>();
-		    boolean check = false;
-		    
-			if (args.length>0) {
-				if (checkWithRegExp(regexpCMD, args[0]))  {
-					check = true;	
-			        for (String arg : args) {
-				      map.put(retFindsVal(p,arg, 1), retFindsVal(p,arg, 2));
-				      }		
-			    }
+		Map<String, String> map = new LinkedHashMap<>();
+
+		if (args.length > 0) {
+
+			for (String arg : args) {
+				if (checkWithRegExp(p, arg)) {
+					map.put(retFindsVal(p, arg, 1), retFindsVal(p, arg, 2));
+				}
 			}
-			if (! check) map.put("HELP", USAGE);
-			return map;
+		}
+		if (!map.containsKey("cmd") | map.containsKey("help")) {
+			map.clear();
+			map.put("HELP", USAGE);
+		}
+		return map;
 	}
-	
-	public static boolean checkWithRegExp(String regexpString, String arg)  {  
-        Pattern p = Pattern.compile(regexpString);  
-        Matcher m = p.matcher(arg);  
-        return m.matches();  
-    }
-	
+
+	public static boolean checkWithRegExp(Pattern p, String arg) {
+		Matcher m = p.matcher(arg);
+		return m.matches();
+	}
+
 	public static String retFindsVal(Pattern p, String arg, int numGroup) {
-	    Matcher m = p.matcher(arg);  
-	    if(m.find())	return m.group(numGroup).replaceAll("=", "").replaceAll("-", ""); 
-	    else return	"";
-	    }	
-	
-	public static void main(String args[]) throws InterruptedException {
-		
-        Map<String, String> map = parsingArgs(args);
-       	     
-	    for (Map.Entry<String, String> entry : map.entrySet()) {
-	    	try {
-	    		executeCmd(entry.getValue());
-				System.out.println(entry.getValue());
-			} catch (InterruptedException e) {}
-      }
-	     
-	  			    
+		Matcher m = p.matcher(arg);
+		if (m.find())
+			return m.group(numGroup).toLowerCase();
+		else
+			return "";
 	}
 
-	
+	public static void main(String args[]) throws InterruptedException {
 
+		Map<String, String> map = parsingArgs(args);
 
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			try {
+				executeCmd(entry.getValue());
+				System.out.println(entry.getValue());
+			} catch (InterruptedException e) {
+			}
+		}
 
+	}
 
 	private static void executeCmd(String map) throws InterruptedException {
 		Runtime runtime = Runtime.getRuntime();
-		
-		try {                                              
-			Process process = runtime.exec(new String[] { "cmd.exe", "/c",  map });
+
+		try {
+			Process process = runtime
+					.exec(new String[] { "cmd.exe", "/c", map });
 			process.waitFor();
 			BufferedReader bReader = new BufferedReader(new InputStreamReader(
 					process.getInputStream()));
@@ -83,5 +77,4 @@ public class Exec {
 		}
 	}
 
-	
 }
--- src/TestExec.java
+++ src/TestExec.java
@@ -1,66 +1,108 @@
-
 import static org.junit.Assert.*;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 
-	public class TestExec{
-		Map<String, String> result;
-		LinkedHashMap<Object, Object> expected;
-	
+public class TestExec {
+	Map<String, String> result;
+	LinkedHashMap<Object, Object> expected;
+
 	@Test
 	public void test() {
-		String args[]={"--cmd=send_message","--user_id=4897","--message=Hello"};
+		String args[] = { "--cmd=send_message", "--user_id=4897",
+				"--message=Hello" };
 		result = Exec.parsingArgs(args);
 		expected = new LinkedHashMap<>();
-		
 		expected.put("cmd", "send_message");
 		expected.put("user_id", "4897");
-		expected.put("message", "Hello");
-		
+		expected.put("message", "hello");
+
 		assertEquals(this.expected, this.result);
 	}
-   
+
 	@Test
 	public void test0() throws InterruptedException {
-		String args[]={"--user_id=4897","--message=Hello"};
-		this.result = Exec.parsingArgs(args);
-		this.expected = new LinkedHashMap<>();
+		String args[] = { "--user_id=4897", "--message=Hello" };
+		result = Exec.parsingArgs(args);
+		expected = new LinkedHashMap<>();
 		expected.put("HELP", Exec.USAGE);
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public void test1() throws InterruptedException {
-		String args[]={"--help=send_message","--user_id=4897","--message=Hello"};
-		this.result = Exec.parsingArgs(args);
-		this.expected = new LinkedHashMap<>();
+		String args[] = { "--help=send_message", "--user_id=4897",
+				"--message=Hello" };
+		result = Exec.parsingArgs(args);
+		expected = new LinkedHashMap<>();
 		expected.put("HELP", Exec.USAGE);
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public void test2() throws InterruptedException {
-		String args[]={"--help"};
-		this.result = Exec.parsingArgs(args);
-		this.expected = new LinkedHashMap<>();
+		String args[] = { "--help" };
+		result = Exec.parsingArgs(args);
+		expected = new LinkedHashMap<>();
 		expected.put("HELP", Exec.USAGE);
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
 	public void test3() {
-		String args[]={};
+		String args[] = {};
+		result = Exec.parsingArgs(args);
+		expected = new LinkedHashMap<>();
+		expected.put("HELP", Exec.USAGE);
+		assertEquals(this.expected, this.result);
+	}
+
+	@Test
+	public void test4() {
+		String args[] = { "--cmd=" };
+		this.result = Exec.parsingArgs(args);
+		this.expected = new LinkedHashMap<>();
+		this.expected.put("HELP", Exec.USAGE);
+		assertEquals(this.expected, this.result);
+	}
+
+	@Test
+	public void test5() {
+		String args[] = { "--user_id=send_message", "--cmd=4897",
+				"--message=Hello" };
+		result = Exec.parsingArgs(args);
+		expected = new LinkedHashMap<>();
+
+		expected.put("user_id", "send_message");
+		expected.put("cmd", "4897");
+		expected.put("message", "hello");
+
+		assertEquals(this.expected, this.result);
+	}
+
+	@Test
+	public void test6() {
+		String args[] = { "--CMD=send_message", "--user_id=4897",
+				"--message=Hello" };
+		result = Exec.parsingArgs(args);
+		expected = new LinkedHashMap<>();
+
+		expected.put("cmd", "send_message");
+		expected.put("user_id", "4897");
+		expected.put("message", "hello");
+
+		assertEquals(this.expected, this.result);
+	}
+
+	@Test
+	public void test7() {
+		String args[] = { "--cmd=send_message", "--help=send_message",
+				"--message=Hello" };
 		this.result = Exec.parsingArgs(args);
 		this.expected = new LinkedHashMap<>();
 		expected.put("HELP", Exec.USAGE);
 		assertEquals(expected, result);
 	}
-	
-	
 }
-
-
