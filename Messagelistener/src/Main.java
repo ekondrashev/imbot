8ebf18852794d0a42001468ce524a5d69a0d06e5
Galusik <gpotemkina@mail.ru>
2014-12-14 13:43:27 +0200

    log4j

--- Messagelistener/src/Main.java
+++ Messagelistener/src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	/**
@@ -7,6 +9,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		try{
+		JOptionPane.showMessageDialog(null, "Hello I RECIEVING");
 		RecvMes.recvMessage();
 		} catch (Exception e)
 		{
--- Messagelistener/src/RecvMes.java
+++ Messagelistener/src/RecvMes.java
@@ -1,11 +1,5 @@
-import java.util.ArrayList;
-
 import java.util.Map;
-import java.util.logging.Logger;
-
-
-
-
+import org.apache.log4j.Logger;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
@@ -14,41 +8,46 @@ import com.rabbitmq.client.QueueingConsumer;
 public class RecvMes {
 
 	private final static String QUEUE_NAME = "Galina_Potemkina";
-	static private boolean chekSTOP = false;
-	private static Logger log = Logger.getLogger(RecvMes.class.getName());
-	
+	static private boolean checkSTOP = false;
+	private static Logger log = Logger.getLogger(RecvMes.class);
+    
 	public static void recvMessage() throws Exception {
 
+				
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("217.146.253.39");
+		//factory.setHost("217.146.253.39");
 		// factory.setHost("192.168.0.81");
+		factory.setHost("192.168.5.195");
 		factory.setPort(5672);
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 		//System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-		log.info("Waiting for messages");
-		
+		log.debug("Waiting for messages");
+
 			
 		QueueingConsumer consumer = new QueueingConsumer(channel);
 		channel.basicConsume(QUEUE_NAME, true, consumer);
 
-		while (chekSTOP == false) {
+		while (checkSTOP == false) {
 			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
 			String message = new String(delivery.getBody());
 			String[] messageArr = message.split("\\s");
 			Map<String, String> map = MyPattern.parsingArgs(messageArr);
+			log.debug("[x] Received '" + messageArr);
 			for (Map.Entry<String, String> entry : map.entrySet()) {
 				if (map.get("cmd").equals("stop"))	{
-					chekSTOP=true;
+					checkSTOP=true;
                 	System.out.println("Process was stopped!");
-                	log.info("Process was stopped!");
+                	log.debug("Process was stopped!");
+                    channel.close();
+                    connection.close();
                    }
 				//System.out.println(" [x] Received '" + entry.getValue() + "'");
-				log.info("[x] Received '" + entry.getValue() + "'");
+				log.debug("[x] Received '" + entry.getValue() + "'");
 			}
-
+        
 		}
 		
 	}
