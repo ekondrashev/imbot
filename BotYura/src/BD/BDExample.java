24178f5ed97d0fcb7ed53568dcf84d9c8b8be1e2
MarkizaSckuza <margaritaq@ukr.net>
2014-12-14 16:33:48 +0200

    modified insert into db

--- BotYura/src/BD/BDExample.java
+++ BotYura/src/BD/BDExample.java
@@ -2,6 +2,7 @@ package BD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -35,21 +36,35 @@ public class BDExample {
 			while(rs.next()){
 				System.out.println(rs.getString("tables_in_imbot"));
 			}
-			st.executeUpdate("insert into Persons (PersonID, LastName, FirstName, Address, City) value (1980,'Gulov', 'Yura', 'no address', 'Odessa')");
-			st.executeUpdate("insert into Persons (PersonID, LastName, FirstName, Address, City) value (2014,'Petrov', 'Vasya', 'no address', 'Odessa')");
+			int PersonID = 1980;
+	        String lastName = "Gulov";
+	        String firstName = "Yura";
+	        String address = "no address";
+	        String city = "Odessa";
+	        
+	        String query = "insert into Persons (PersonID, LastName, FirstName, Address, City) values (?, ?, ?, ?, ?)";
+	        PreparedStatement preparedStmt = c.prepareStatement(query);
+	        
+	        preparedStmt.setInt(1, PersonID);
+	        preparedStmt.setString(2, lastName);
+	        preparedStmt.setString(3, firstName);
+	        preparedStmt.setString(4, address);
+	        preparedStmt.setString(5, city);
+	        preparedStmt.execute();
+	        
 			
 			rs = st.executeQuery("select * from Persons");
 			
 			while (rs.next())
 		      {
-		        int id = rs.getInt("PersonID");
-		        String lastName = rs.getString("LastName");
-		        String firstName = rs.getString("FirstName");
-		        String address = rs.getString("Address");
-		        String city = rs.getString("City");
+		        int id1 = rs.getInt("PersonID");
+		        String lastName1 = rs.getString("LastName");
+		        String firstName1 = rs.getString("FirstName");
+		        String address1 = rs.getString("Address");
+		        String city1 = rs.getString("City");
 		         
 		        // print the results
-		        System.out.format("%s, %s, %s, %s, %s\n", id, firstName, lastName, address, city);
+		        System.out.format("%s, %s, %s, %s, %s\n", id1, firstName1, lastName1, address1, city1);
 		      }
 		      st.close();
 			
