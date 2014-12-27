634eb99b8c1fe05b39daec2719d299b744a73854
Потемкин Алексей <potemkin_81@ukr.net>
2014-12-27 18:27:49 +0200

    Application for work with skype and sent to DB application

---
+++ SkypeJavaBot2/src/skypePackage/Recv.java
@@ -0,0 +1,102 @@
+package skypePackage;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.log4j.Logger;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+import com.skype.Chat;
+import com.skype.ChatMessage;
+
+public class Recv implements Runnable{
+	private static final Logger log = Logger.getLogger(Recv.class);
+
+	private static String QUEUE_NAME = "QUEUE";
+	private static String HOST = "127.0.0.1";
+	static private boolean shutdownFlag = false;
+
+	Map<String, ChatMessage> map = new LinkedHashMap<>();
+
+	Recv(String queue, String host, Map<String, ChatMessage> map) {
+		QUEUE_NAME = queue;
+		HOST = host;
+		log.info("Initialization class Recv");
+		log.info("Initialization queue:" + QUEUE_NAME);
+		log.info("Initialization HOST:" + HOST);
+		this.map = map;
+	}
+
+	public void recive() throws Exception {
+		Connection connection = null;
+		ConnectionFactory factory;
+		try {
+			log.info("Start processing  My HOST=" + HOST + " my queue="
+					+ QUEUE_NAME);
+			factory = new ConnectionFactory();
+			factory.setHost(HOST);
+			connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out
+					.println(" [*] Waiting for messages. To exit press CTRL+C");
+			log.info(" [*] Waiting for messages. To exit press CTRL+C");
+
+			QueueingConsumer consumer = new QueueingConsumer(channel);
+			channel.basicConsume(QUEUE_NAME, true, consumer);
+
+			while (false == shutdownFlag) {
+				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+				String message = new String(delivery.getBody());
+				System.out.println(" [x] Received '" + message + "'");
+				log.info(" [x] Received '" + message + "'");
+				ParsingRegulary exec = new ParsingRegulary();
+				String[] arg = message.split("\\^");
+				Map<String, String> mapDecode = exec.decodeToMap(arg);
+
+				if (mapDecode.get("command").equals("stop")) {
+					shutdownFlag = true;
+					log.info("command:stop");
+				}
+
+				else if (mapDecode.get("command").equals("print")) {
+					String ourMessage = "";
+					for (Entry<String, String> elem : mapDecode.entrySet()) {
+						ourMessage = elem.getKey() + ":" + elem.getValue()
+								+ "\n";
+						log.info(ourMessage);
+					}
+				} else if (mapDecode.get("command").equals("answer")) {
+					log.info("get command:"+mapDecode.get("command"));
+					ChatMessage chat = this.map.get(mapDecode.get("key"));
+					final Chat chatterup = chat.getChat();
+					chatterup.send(mapDecode.get("text").replaceAll("\"", ""));
+					this.map.remove(mapDecode.get("key"));
+				}
+
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw e;
+		} finally {
+			if (connection != null)
+				connection.close();
+			log.info("close all connection");
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			recive();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
--- SkypeJavaBot2/src/skypePackage/SkypeMain.java
+++ SkypeJavaBot2/src/skypePackage/SkypeMain.java
@@ -1,17 +1,30 @@
 package skypePackage;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import com.skype.ChatMessage;
 import com.skype.Skype;
 import com.skype.SkypeException;
 
 public class SkypeMain {
 
-	public static void main(String[] args) throws SkypeException {
+	public static void main(String[] args) throws SkypeException, InterruptedException {
+		JOptionPane.showMessageDialog(null, "Start listener");
+		Map<String,ChatMessage> map=new LinkedHashMap<>();
 		
 		new SkypeMain();
 		
-		Skype.addChatMessageListener(new MyListener());
+		Skype.addChatMessageListener(new MyListener(map));
 		Skype.setDaemon(false);
 		
+		Recv rdRecive = new Recv("FromDB","127.0.0.1",map);
+		Thread threadRecive = new Thread(rdRecive);
+		threadRecive.setName("Recive");
+		threadRecive.start();
+		threadRecive.join();
 	}
 
 }
