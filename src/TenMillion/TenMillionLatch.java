ef7da9dcd5363436ad11be66db3f21efda11800d
Denis <denissaranuk@gmail.com>
2015-02-01 16:09:38 +0200

    TenMillion+TenmillionLatch+test for both

---
+++ src/TenMillion/TenMillionLatch.java
@@ -0,0 +1,34 @@
+package TenMillion;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TenMillionLatch extends Thread {
+	public static void main(String[] args) throws InterruptedException {
+		final oneMillion tenThreads = new oneMillion();
+
+		final CountDownLatch doneSignal = new CountDownLatch(10);
+
+		for (int i = 0; i < 10; ++i)
+		// create and start threads
+
+		{
+			Thread thready = new Thread() {
+				public void run() {
+					synchronized (tenThreads) {
+						tenThreads.millionCount();
+						doneSignal.countDown();
+					}
+
+				}
+
+			};
+
+			thready.start();
+			
+
+		}
+		doneSignal.await();
+
+	}
+
+}
--- src/TenMillion/test.java
+++ src/TenMillion/test.java
@@ -5,12 +5,21 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class test extends TestCase {
+
+	@Test
+	public void testTenMillionLatch() throws InterruptedException {
+		oneMillion.x = 0;
+		TenMillionLatch.main(null);
+		int expected = 10000000;
+		int actual = oneMillion.x;
+		assertEquals(expected, actual);
+	}
 	@Test
 	public void testTenMillion() throws InterruptedException {
+		oneMillion.x = 0;
 		tenMillionCounter.main(null);
 		int expected = 10000000;
 		int actual = oneMillion.x;
 		assertEquals(expected, actual);
 	}
-
 }
