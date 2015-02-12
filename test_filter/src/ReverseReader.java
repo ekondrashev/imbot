ff6540f00ee58c2b1b6079b46ee604f6ac764ed9
ek <a.a.piluck@gmail.com>
2015-02-12 21:12:30 +0200

    Added test for the filter

---
+++ test_filter/src/ReverseReader.java
@@ -0,0 +1,12 @@
+import java.io.FilterReader;
+import java.io.Reader;
+
+
+public class ReverseReader extends FilterReader {
+
+	public ReverseReader(Reader in) {
+		super(in);
+		// TODO Auto-generated constructor stub
+	}
+
+}
