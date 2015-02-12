44fa7bb95e925461b988843d9ab6ed63c941f020
Yura <Yura@01>
2015-02-12 21:12:54 +0200

    Do home work

--- ReverseReader/src/ReverseReader.java
+++ ReverseReader/src/ReverseReader.java
@@ -5,8 +5,16 @@ import java.io.Reader;
 
 public class ReverseReader extends FilterReader{
 
+	protected StringBuilder str = new StringBuilder();
 	protected ReverseReader(Reader in) throws IOException {
 		super(in);
-		
+		int i;
+		while ((i = in.read()) != -1){
+			str.append(i);
+		}
+	}
+	
+	public int read(){
+		return 0;
 	}
 }
