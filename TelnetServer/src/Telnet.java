0517f989bba42bb54eb0c88dc88b261cae5f81d0
Yura <Yura@01>
2015-02-15 16:19:48 +0200

    two servers

--- TelnetServer/src/Telnet.java
+++ TelnetServer/src/Telnet.java
@@ -10,32 +10,46 @@ public class Telnet {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		ServerSocket serverSocket = null;
-		PrintWriter out = null;
-		BufferedReader in = null;
+		ServerSocket serverSocket = null, serverSocket2 = null;
+		PrintWriter out = null, out2 = null;
+		BufferedReader in = null, in2 = null;
 		
-		int portNumber =Integer.parseInt("9000");
+		int portNumber = Integer.parseInt("9000");
+		int portNumber2 = Integer.parseInt("9001");
 		
 		try{
 			serverSocket = new ServerSocket(portNumber);
+			serverSocket2 = new ServerSocket(portNumber2);
 			Socket clientSocket = serverSocket.accept();
+			Socket clientSocket2 = serverSocket2.accept();
 			System.out.println("Got connection");
 			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			out2 = new PrintWriter(clientSocket2.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			String str;
-			while (!(str = in.readLine()).equals("exit")){
-				System.out.println("Got msg: " + str );
-			out.write("You wrote: " + str);
+			in2 = new BufferedReader(new InputStreamReader(clientSocket2.getInputStream()));
+			String str = ""; 
+			String str2 = "";
+			while (!(str = in.readLine()).equals("exit") || !(str2 = in2.readLine()).equals("exit")){
+				//System.out.println("Got msg: " + str );
+			out.write("You wrote: " + str + str2 + "\n\r");
+			out2.write("You wrote: " + str + str2 + "\n\r");
 			out.flush();
+			out2.flush();
 			}
 					
 		} finally {
 			if (serverSocket != null)
 				serverSocket.close();
+			if (serverSocket2 != null)
+				serverSocket2.close();
 			if (out != null)
 				out.close();
+			if (out2 != null)
+				out2.close();
 			if (in != null)
 				in.close();
+			if (in2 != null)
+				in2.close();
 		}
 	}
 }
