698e06e1647844387effc88e5528a09d1e7578e1
Aleksandr Lobovko <Wade_3_3@mail.ru>
2015-02-15 16:31:27 +0200

    Networking example

---
+++ NetwotkingExample/src/NetClient.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class NetClient {
+	public static void main(String[] args) throws IOException {
+		String inRead;
+		int portNumber = 4530;
+
+		try (ServerSocket serverSocket = new ServerSocket(portNumber);
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter out = new PrintWriter(
+						clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						clientSocket.getInputStream()));) {
+			out.println("Hello");
+
+			while ((inRead = in.readLine()) != null)
+				if (!inRead.startsWith("exit"))
+					out.println("Return mess: " + inRead);
+				else
+					out.close();
+			out.close();
+			in.close();
+			clientSocket.close();
+			serverSocket.close();
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: " + portNumber);
+			System.exit(1);
+		}
+
+	}
+}
