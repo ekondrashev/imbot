c07a26c982ce84de3dee968ca371315f1e30ab32
plumberYura <yuraodesit@yandex.ru>
2015-02-20 15:46:13 +0200

    Don't work

--- LocalServer/src/TrainingClient.java
+++ LocalServer/src/TrainingClient.java
@@ -1,22 +1,70 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.logging.Logger;
 
-public class TrainingClient extends TrainingServer {
+public class TrainingClient implements Runnable{
 
-	public TrainingClient(String host, int port, String user) {
-		super();
-		try {
-			clientSocket = new Socket(host, port);
-			initOutInFields();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("You have connected to " + host);
-		userName = user;
+    Logger log = Logger.getLogger(TrainingServer.class.getName());
+    int port;
+    String host;
+    protected String userName;
+    protected String clientName;
+
+    public TrainingClient(String host, int port, String user) {
+	this.port = port;
+	userName = user;
+	this.host = host;
+    }
+
+    @Override
+    public void run() {
+
+	try (Socket clientSocket = new Socket(host, port)) {
+	    System.out.println("You have connected");
+	    try (PrintWriter out = new PrintWriter(
+		    clientSocket.getOutputStream(), true);
+		    BufferedReader in = new BufferedReader(
+			    new InputStreamReader(clientSocket.getInputStream()));) {
+		
+		Thread listen = new Thread() {
+		    public void run() {
+			try {
+			    while (true) {
+				System.out.println(in.readLine());
+			    }
+			} catch (IOException e) {
+			    // TODO Auto-generated catch block
+			    System.out.println(e.getMessage());
+			}
+		    }
+		};
+		listen.start();
+
+		Thread sender = new Thread() {
+		    public void run() {
+			BufferedReader localReader = new BufferedReader(
+				new InputStreamReader(System.in));
+			String str = null;
+			try {
+			    while (!(str = localReader.readLine())
+				    .equals("exit")) {
+				out.println(str);
+			    }
+			    log.info("Close connection");
+			} catch (IOException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			}
+		    }
+		};
+		sender.start();
+
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
 }
--- LocalServer/src/TrainingServer.java
+++ LocalServer/src/TrainingServer.java
@@ -7,95 +7,76 @@ import java.net.Socket;
 import java.util.logging.Logger;
 
 public class TrainingServer implements Runnable {
-	Logger log = Logger.getLogger(TrainingServer.class.getName());
-	protected ServerSocket serverSocket = null;
-	protected Socket clientSocket = null;
-	protected PrintWriter out = null;
-	protected BufferedReader in = null;
-	protected String userName;
-	protected String clientName;
+    Logger log = Logger.getLogger(TrainingServer.class.getName());
+    int port;
+    protected String userName;
+    protected String clientName;
 
-	protected TrainingServer() {
+    protected TrainingServer() {
 
-	}
+    }
 
-	public TrainingServer(int port, String user) {
-		try {
-			serverSocket = new ServerSocket(port);
-			System.out.println("Server is running and waits for a connection .");
-			clientSocket = serverSocket.accept();
-			initOutInFields();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		userName = user;
-	}
+    public TrainingServer(int port, String user) {
+	this.port = port;
+	userName = user;
+    }
 
-	protected void initOutInFields() throws IOException {
-		out = new PrintWriter(clientSocket.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(
-				clientSocket.getInputStream()));
-	}
-
-	@Override
-	public void run() {
-		out.println(userName); // Send my name to companion
-		try {
-			clientName = in.readLine();// Get companion's name
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			System.out.println(e1.getMessage());
-		}
-		System.out.println("Let's speak with " + clientName);
-		
-		Thread listen = new Thread() {
-			public void run() {
+    @Override
+    public void run() {
+	
+	try (ServerSocket serverSocket = new ServerSocket(port)) {
+	    System.out
+		    .println("Server is running and waits for a connection .");
+	    //while (true) {
+		try (Socket clientSocket = serverSocket.accept()) {
+		    try (PrintWriter out = new PrintWriter(
+			    clientSocket.getOutputStream(), true);
+			    BufferedReader in = new BufferedReader(
+				    new InputStreamReader(
+					    clientSocket.getInputStream()));) {
+			
+			Thread listen = new Thread() {
+			    public void run() {
 				try {
-					while (true) {
-						System.out.println(clientName + " - " + in.readLine());
-					}
+				    while (true) {
+					System.out.println(in.readLine());
+				    }
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					System.out.println(e.getMessage());
+				    // TODO Auto-generated catch block
+				    System.out.println(e.getMessage());
 				}
-			}
-		};
-		listen.setDaemon(true);
-		listen.start();
+			    }
+			};
+			listen.start();
 
-		Thread sender = new Thread() {
-			public void run() {
+			Thread sender = new Thread() {
+			    public void run() {
 				BufferedReader localReader = new BufferedReader(
-						new InputStreamReader(System.in));
+					new InputStreamReader(System.in));
 				String str = null;
 				try {
-					while (!(str = localReader.readLine()).equals("exit")) {
-						out.println(str);
-					}
-					log.info("Close connection");
+				    while (!(str = localReader.readLine())
+					    .equals("exit")) {
+					out.println(str);
+				    }
+				    log.info("Close connection");
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				    // TODO Auto-generated catch block
+				    e.printStackTrace();
 				}
-			}
-		};
-		sender.start();
-	}
+			    }
+			};
+			sender.start();
 
-	protected void finalize() {
-		try {
-			if (out != null)
-				out.close();
-			if (in != null)
-				in.close();
-			if (clientSocket != null)
-				clientSocket.close();
-			if (serverSocket != null)
-				serverSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		    }
+		} catch (Exception e) {
+		    e.printStackTrace();
 		}
+	  //  }
+
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
+
 }
